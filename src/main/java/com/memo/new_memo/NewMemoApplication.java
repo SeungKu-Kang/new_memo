c488f7d7b3f33788f2abd244e33a07f9e38316e5
DESKTOP-HBIBUUM\user <test@test.com>
2024-08-01 18:42:11 +0900

    0801 환경 세팅 완료 및 TestController를 통한 기본테스트 완료 , 기본 마크업 작성 중

---
+++ src/main/java/com/memo/new_memo/NewMemoApplication.java
@@ -0,0 +1,13 @@
+package com.memo.new_memo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewMemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewMemoApplication.class, args);
+	}
+
+}
